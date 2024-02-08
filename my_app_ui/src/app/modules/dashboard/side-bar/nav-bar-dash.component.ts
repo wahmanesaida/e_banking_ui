07343b057d5fa0069d9474ed21b5b28879bbacae
Oumaima-Daoudi <daoudioumaima300@gmail.com>
2024-02-08 21:51:32 +0100

    implement the sideBar

--- my_app_ui/src/app/modules/dashboard/side-bar/nav-bar-dash.component.ts
+++ my_app_ui/src/app/modules/dashboard/side-bar/nav-bar-dash.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'side-bar',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar-dash.component.css'
 })
 export class NavBarDashComponent {
+  faArrowLeft = faArrowLeft;
+
+
+  @ViewChild('sidebar') sidebar: ElementRef<HTMLElement>;
+  @ViewChild('closeBtn') closeBtn: ElementRef<HTMLElement>;
+
+  toggleSidebar() {
+    this.sidebar.nativeElement.classList.toggle("open");
+    this.menuBtnChange();
+  }
+
+  menuBtnChange() {
+    if (this.sidebar.nativeElement.classList.contains("open")) {
+      this.closeBtn.nativeElement.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+      this.closeBtn.nativeElement.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+  }
+
 
 }
