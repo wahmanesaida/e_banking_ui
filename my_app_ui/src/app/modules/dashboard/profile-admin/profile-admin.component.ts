a5b09bab1eaf806e00540c1ce9b16d180822730c
Oumaima-Daoudi <daoudioumaima300@gmail.com>
2024-05-05 07:25:03 +0100

    modify the admin profile

--- my_app_ui/src/app/modules/dashboard/profile-admin/profile-admin.component.ts
+++ my_app_ui/src/app/modules/dashboard/profile-admin/profile-admin.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users/users.service';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-profile-admin',
   templateUrl: './profile-admin.component.html',
   styleUrl: './profile-admin.component.css'
 })
-export class ProfileAdminComponent {
+export class ProfileAdminComponent implements OnInit {
+
+  constructor(private userService:UsersService, private toast: NgToastService) {
+  }
+
+  ngOnInit(): void {    
+  }
+
+  getAdminProfile(id:number){
+    this.userService.getAdminProfile(id).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error: any) => {
+        this.toast.error({
+          detail: 'Pay Attention',
+          summary: error.error,
+          duration: 5000,
+          position: 'topCenter',
+        });
+      }
+    );
+  }
+
+
 
 }
